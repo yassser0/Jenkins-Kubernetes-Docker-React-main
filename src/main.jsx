a5f6df6a22d7942bd0ba4659a2d6429feb6646fb
yasser rachih <yassirach17@gmail.com>
2025-03-08 15:30:03 +0000

    all files

--- src/main.jsx
+++ src/main.jsx
@@ -172,7 +172,7 @@ const App = () => {
         ))}
       </ul>
       {todos.length > 0 && (
-        <button onClick={clearTodos} style={styles.clearButton}>Clear All Todos</button>
+        <button onClick={clearTodos} style={styles.clearButton}>Clear All</button>
       )}
     </div>
   );
